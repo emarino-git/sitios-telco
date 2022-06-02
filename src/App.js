244aaa855ba5e05eb1352f522ae1c0d295de962f
Matias Marino <marinomatias@outlook.com.ar>
2022-06-02 18:24:11 -0300

    first commit

--- src/App.js
+++ src/App.js
@@ -7,7 +7,7 @@ function App() {
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <p>
-          Edit <code>src/App.js</code> and save to reload.
+          Busqueda de documetación por Sitio
         </p>
         <a
           className="App-link"
