32f4ab41b4efdb92a9ca6db62699aa09094a2e97
emarino-git <82189750+emarino-git@users.noreply.github.com>
2022-06-29 04:15:01 +0000

    adding bing map

--- src/App.js
+++ src/App.js
@@ -15,6 +15,14 @@ function App() {
         >
           CR Cazador
         </a>
+        <div>
+          <iframe width="500" height="300" frameborder="0" src="https://www.bing.com/maps/embed?h=300&w=500&cp=-34.53639175539835~-58.484577538155406&lvl=18&typ=d&sty=r&src=SHELL&FORM=MBEDV8" scrolling="no">
+          </iframe>
+          <div>
+            <a id="largeMapLink" target="_blank" href="https://www.bing.com/maps?cp=-34.53639175539835~-58.484577538155406&amp;sty=r&amp;lvl=17.117753950046435&amp;FORM=MBEDLD">Maximizar</a> &nbsp; | &nbsp;
+            <a id="dirMapLink" target="_blank" href="https://www.bing.com/maps/directions?cp=-34.53639175539835~-58.484577538155406&amp;sty=r&amp;lvl=17.117753950046435&amp;rtp=~pos.-34.53639175539835_-58.484577538155406____&amp;FORM=MBEDLD">Cómo llegar</a>
+          </div>
+        </div>
         <ul>
           <li>Localidad: El Cazador</li>
           <li>Partido: Escobar</li>
